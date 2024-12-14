e4ea86965e02004a65ce8ffa302fa0aa68360a07
nathnael zelalem <nathnaelzelalem@gmail.com>
2024-12-14 03:23:19 -0800

    Back end for vehicle mgmt system

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const carRoute = require("./router/carRoute");
+dotenv.config({ path: "./config.env" });
+const app = express();
+const corsOptions = {
+  origin: process.env.CLIENT_URL || "*",
+  optionsSuccessStatus: 200,
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use("/", (req, res, next) => {
+  console.log("Hello");
+  next();
+});
+app.use("/api/car", carRoute);
+
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log("Monogo Db Connected Successfully !!!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`this server Run on Server on port :${PORT}`);
+});
